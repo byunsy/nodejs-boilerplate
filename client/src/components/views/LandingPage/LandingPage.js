1873d93065480dfffbf926296c9b679a7d876455
unknown <bsyiskl@gmail.com>
2021-03-17 22:15:24 +0900

    initial commit client-side

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import React, {useEffect} from 'react'
+import { withRouter } from 'react-router-dom';
+
+function LandingPage(props) {
+
+    useEffect(() => {
+        axios.get('api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+        .then(response => {
+            if(response.data.success) {
+                props.history.push('/login')
+            } else {
+                alert("Failed to sign out.")
+            }
+        })
+    }
+
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center', 
+            width: '100%', height: '95vh'
+        }}>
+            <h1>Landing Page</h1>
+
+            <button onClick={onClickHandler}>
+                Sign Out
+            </button>
+        </div>
+    )
+}
+
+export default withRouter (LandingPage)
