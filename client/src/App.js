6c5c3fb4d6eb901cf5aa1ae0b2df02bc357698ce
unknown <bsyiskl@gmail.com>
2021-03-19 22:10:28 +0900

    added navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,21 @@ import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 import Auth from "./hoc/auth";
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
 
-const { Content } = Layout;
+const { Header, Content } = Layout;
 
 function App() {
   return (
     <div>
+      {/* <Header style={{ padding: 0, background: "white" }}> */}
+      <Menu mode="horizontal" defaultSelectedKeys={["1"]}>
+        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="2">Nav 2</Menu.Item>
+        <Menu.Item key="3">Nav 3</Menu.Item>
+      </Menu>
+      {/* </Header> */}
+
       <Content>
         <Router>
           <Switch>
